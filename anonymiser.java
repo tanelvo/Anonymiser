76589632d97aa4f6e1cba5454aeb4945e9cb0c47
Tanel Volkov <tanel.volkov@aktors.ee>
2024-02-15 17:49:04 +0200

    .

--- anonymiser.java
+++ anonymiser.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class anonymiser {
+
+    public static String[] splitStringIntoArray(String input) {
+        // Split the input string into an array of words
+        String[] words = input.split("\\s+");
+        String pepu = findFirstInSentence(words);
+        return words;
+    }
+
+    public static String findFirstInSentence(String[] input) {
+        //String[] info;
+        System.out.println("firstInSentence");
+        List<String> firstInSentence = new ArrayList<>();
+        for (int i = 1; i < input.length; i++) {
+            if(input[i].matches(".*[A-Z].*")){
+                if(input[i-1].endsWith(".")) {
+                    firstInSentence.add(input[i]);
+                    System.out.println(firstInSentence);
+                }
+            }
+        }
+        return "info";
+    }
 
     private static final String NAME_REGEX = "\\b[A-Z][a-z]*\\b";
     private static final String ADDRESS_REGEX = "\\b\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)\\b";
@@ -37,10 +61,18 @@ public class anonymiser {
     public static void main(String[] args) {
         String text = "Kiisu nimega Mirjam Miisu elab aadressil Valge 16, Tallinn. Mirjamile ei meeldi oma naaber Juhan, kes mjäugub liiga valjult. Mirjam on 4 aastane isikukoodiga 62001240838. Mirjamiga saab ühendust võtta numbril +37259108902 või mirjam.miisu@gmail.com.";
         String[] personalInfo = identifyPersonalInfo(text);
+
+        String[] textArray = splitStringIntoArray(text);
         
         // Print the identified personal information
-        for (String info : personalInfo) {
+        /* for (String info : personalInfo) {
             System.out.println(info);
         }
+        
+        System.out.println("-----------------");
+
+        for (String info : textArray) {
+            System.out.println(info);
+        } */
     }
 }
