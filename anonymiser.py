ebd85f7deb2f656a9e23f8a3c97d2d2718f5260a
Tanel Volkov <tanel.volkov@aktors.ee>
2024-02-20 17:51:52 +0200

    Script to combine I to B

--- anonymiser.py
+++ anonymiser.py
@@ -2,40 +2,74 @@ from transformers import BertTokenizer, BertForTokenClassification
 from transformers import pipeline
 from docx import Document
 
-# Eemaldada väikse esitähega sõnad, et ruumi säästa? 
-def remove_non_capitalized_words(input_string):
-    # Split the input string into words
-    words = input_string.split()
-
-    # Filter out words that don't start with a capitalized letter
-    filtered_words = [word for word in words if word[0].isupper()]
-
-    # Reconstruct the string from filtered words
-    filtered_string = ' '.join(filtered_words)
-
-    return filtered_string
-
 tokenizer = BertTokenizer.from_pretrained('tartuNLP/EstBERT_NER')
 bertner = BertForTokenClassification.from_pretrained('tartuNLP/EstBERT_NER')
 
 nlp = pipeline("ner", model=bertner, tokenizer=tokenizer)
-sentence = 'Hispaania President on Ivan Frolov. Ma elan Sakus, Lasnamäel on mul ka maja. Sündisin 12.05.2007. Töötan firmas nimega Aktors OÜ.'
 
+class NerResult:
+    def __init__(self, entity, word, score, index, start, end):
+        self.entity = entity
+        self.word = word
+        self.score = score
+        self.index = index
+        self.start = start
+        self.end = end
+
+# Read the content of the Word document
 docx_file_path = './essee.docx'
 doc = Document(docx_file_path)
+text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
 
-sentence = ""
-for paragraph in doc.paragraphs:
-    sentence += paragraph.text + "\n"
-print(sentence)
-sentence = sentence.strip()
+ner_person = []
+ner_organisation = []
+ner_location = []
 
-sentence = remove_non_capitalized_words(sentence)
+def move_hashtag_words(array):
+    i = 0
+    while i < len(array) - 1:  # Iterate up to the second-to-last element
+        if array[i+1]['entity'].startswith('I'):
+            if array[i+1]['word'].startswith('##'):
+                array[i]['word'] = array[i]['word'] + array[i+1]['word'].strip("#")
+            else:
+                array[i]['word'] = array[i]['word'] + ' ' + array[i+1]['word']
+            del array[i+1]
+        else:
+            i += 1  # Move to the next element if no deletion is done
 
-ner_results = nlp(sentence)
-print(ner_results)
-for result in ner_results:
-    print(result)
+def categorize(results):
+    for result in results:
+        if result['entity'] in ['B-PER', 'I-PER']:
+            ner_person.append(result)
+        elif result['entity'] in ['B-ORG', 'I-ORG']:
+            ner_organisation.append(result)
+        elif result['entity'] in ['B-LOC', 'I-LOC']:
+            ner_location.append(result)
+    
+    move_hashtag_words(ner_organisation)
+    
 
+# Split the text into chunks and process each chunk individually
+max_chunk_length = 512
 
+for i in range(0, len(text), max_chunk_length):
+    chunk = text[i:i + max_chunk_length]
+    chunk = chunk.strip()  # Remove leading and trailing whitespace
+
+    # Perform NER on the chunk
+    ner_results = nlp(chunk)
+    #print(ner_results)
+    categorize(ner_results)
+
+#print("Persons:", ner_person)
+for per in ner_person:
+    print(per)
+print("------------------------------------------------------------")
+for org in ner_organisation:
+    print(org)
+print("------------------------------------------------------------")
+for loc in ner_location:
+    print(loc)
+#print("Organisations:", ner_organisation)
+#print("Locations:", ner_location)
 
